a2b9918895e0d8cff94be5bbda180128e2105e85
mengxiangyu <mengxiangyu5@btte.net>
2018-09-20 17:40:38 +0800

    this is HelloWorld.java file commit mark use -m option

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,8 @@ public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hello World!");  //Êä³ö Hello World
 		System.out.println("Hello World!");
+
+
+		System.out.println("WorkHard!");
 	}
 }
